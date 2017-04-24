5f8c842768f898523e9027f76b9542434f4a3cba
Connor Larsen and Isabella Abatgis <connor@>
2017-04-24 09:53:27 -0700

    add JS and HTML head.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+}
+
+var number1 = parseInt(prompt("Choose a number: "));
+var number2 = parseInt(prompt("Choose another number: "));
+
+var result = add(number1, number2);
+alert(result);
